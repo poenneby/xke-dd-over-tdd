22656835accf2bf5170c9f47f590f97822eeaae5
poenneby <peter@onneby.com>
2016-01-12 00:27:38 +0100

    All tests passing, Done

--- spec/dungeon_spec.rb
+++ spec/dungeon_spec.rb
@@ -1,11 +1,19 @@
+require_relative '../lib/converters.rb'
+
 class Dungeon
-  def itemFor(name)
-    "Medal"
+  def itemFor(name_in)
+    total = 0
+    name_in.upcase.each_char{ |letter| total += Converters.toNumber(letter) } 
+    return Converters.toItem(Converters.toLetter(Converters.reduce(total)))
   end
 end
 
 describe "The Dungeon" do
-  xit "Akuku gets a Medal" do
+  it "Akuku gets a Medal" do
     expect(Dungeon.new.itemFor("Akuku")).to eq "Medal"
+  end
+
+  it "Sofia gets Boots" do
+    expect(Dungeon.new.itemFor("Sofia")).to eq "Boots"
   end
 end
